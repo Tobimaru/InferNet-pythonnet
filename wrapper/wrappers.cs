79cbea5641c09f67a14887d5bc7c8df14b3bbb39
Tobimaru <honfai.choi@gmail.com>
2019-03-24 22:36:46 +0100

    add csharp wrapper infer.net

---
+++ wrapper/wrappers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Mod = Microsoft.ML.Probabilistic.Models;
+
+namespace InferWrapper
+{
+    public static class Variable
+    {
+        public static Mod.VariableArray<T> Observed<T>(T[] arr)
+        {
+            return Mod.Variable.Observed(arr);
+        }
+    }
+}
